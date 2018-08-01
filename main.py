16949ac8b4139e0384db6e66cb61f2c5863134a9
Peter Kazarinoff <peter.kazarinoff@gmail.com>
2018-07-31 22:22:15 -0700

    adds

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # main.py runs after boot.py
 
-import run
+#import run
+import run2
 
 try:
-    run.main()
+    #run.main()
+    run2.main()
 except:
     pass
 
--- run2.py
+++ run2.py
@@ -1,22 +1,24 @@
 # ESP8266 Feather Huzzah Weather Station, connected to flask IoT server
 
 import wifitools
-import MCP9808
+import BMP280
 import time
 import config
+import machine
 
 def main():
     ssid = config.SSID
     password = config.WIFI_PASSWORD
     api_key = config.API_KEY
     mac_address = config.MAC_ADDRESS
-    field = '1'
-
+    field = '2'                         # 2nd temperature sensor is field 2
     wifitools.connect(ssid,password)
     time.sleep(5)
+    i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
+    sensor = BMP280.BMP280(i2c)
 
     for i in range(60*8):
-        data = MCP9808.readtemp()
+        data = str(sensor.values[0])
         try:
             wifitools.flaskiot_post(api_key,mac_address,field,data)
         except:
