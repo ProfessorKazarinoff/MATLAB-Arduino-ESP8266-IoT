4fa6e0177cd66a3d3129a7c3e0e8ab12c2b9414b
Peter Kazarinoff <peter.kazarinoff@gmail.com>
2018-09-07 10:33:20 -0700

    adds

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# main.py runs after boot.py
+# main.py
+# runs after boot.py
 
-#import run
-import run2
+import run
+#import run2
 
 try:
-    #run.main()
-    run2.main()
+    run.main()
+    #run2.main()
 except:
     pass
-
